87b90a0559555c349e6b6dc6b996678c19c5e499
Yehan Wasura <yehantest@gmail.com>
2021-12-31 16:50:20 +0000

    Add files via upload

--- Player/player.gd
+++ Player/player.gd
@@ -1,19 +1,18 @@
 extends KinematicBody2D
 
 var speed = 400
-const NORMAL_POSITION = Vector2(100, 300)
 const GAME_SIZE = Vector2(1280, 720)
 	
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_touch"):
 		var input_position = get_global_mouse_position()
-		if NORMAL_POSITION.y < input_position.y:
-			velocity.y += 1.5
-		elif NORMAL_POSITION.y > input_position.y:
-			velocity.y -= 1.5
+		if position.y < input_position.y:
+			velocity.y += min(1.5 * speed, (input_position.y - position.y) / delta)
+		elif position.y > input_position.y:
+			velocity.y -= min(1.5 * speed, (position.y - input_position.y) / delta)
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y -= 1 * speed
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	move_and_slide(velocity * speed)
+		velocity.y += 1 * speed
+	move_and_slide(velocity)
